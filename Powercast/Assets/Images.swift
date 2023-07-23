5b95b9aac1ef19a2c259cc9b5eef9266e60bcc42
Jakob Jensen <jakobj@jakobj.dk>
2023-07-23 14:39:02 +0200

    update

--- Powercast/Assets/Images.swift
+++ Powercast/Assets/Images.swift
@@ -2,7 +2,8 @@
 import UIKit
 struct Images {
 	static let dk1 = UIImage(named:"dk1")!
-	static let powercast_splash = UIImage(named:"powercast-splash")!
 	static let dk2 = UIImage(named:"dk2")!
 	static let offshore_wind_power = UIImage(named:"offshore-wind-power")!
+	static let powercast_splash = UIImage(named:"powercast-splash")!
 }
+// swiftlint:enable all
