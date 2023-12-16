b4cf2b98a79134d41507ae7b99a3319769fdd0f3
Jakob Jensen <jakobj@jakobj.dk>
2023-12-17 00:39:39 +0100

    Make menu prettier (#50)

--- Powercast/Assets/Translations.swift
+++ Powercast/Assets/Translations.swift
@@ -1,6 +1,7 @@
 // swiftlint:disable all
 import Foundation
 struct Translations {
+	static func COMMIT_LABEL(_ p1: String) -> String { return NSLocalizedString("COMMIT_LABEL", comment: "").replacingOccurrences(of: "%1", with: p1) }
 	static let DASHBOARD_CO2_LABEL = NSLocalizedString("DASHBOARD_CO2_LABEL", comment: "")
 	static func DASHBOARD_CO2_SPAN(_ p1: String, _ p2: String) -> String { return NSLocalizedString("DASHBOARD_CO2_SPAN", comment: "").replacingOccurrences(of: "%1", with: p1).replacingOccurrences(of: "%2", with: p2) }
 	static let DASHBOARD_CO2_UNIT = NSLocalizedString("DASHBOARD_CO2_UNIT", comment: "")
@@ -78,7 +79,7 @@ struct Translations {
 	static let SETTINGS_SYSTEM_TITLE = NSLocalizedString("SETTINGS_SYSTEM_TITLE", comment: "")
 	static let SETTINGS_TITLE = NSLocalizedString("SETTINGS_TITLE", comment: "")
 	static let UPGRADE_IS_REQUIRED = NSLocalizedString("UPGRADE_IS_REQUIRED", comment: "")
-	static func VERSION_LABEL(_ p1: String, _ p2: String, _ p3: String) -> String { return NSLocalizedString("VERSION_LABEL", comment: "").replacingOccurrences(of: "%1", with: p1).replacingOccurrences(of: "%2", with: p2).replacingOccurrences(of: "%3", with: p3) }
+	static func VERSION_LABEL(_ p1: String, _ p2: String) -> String { return NSLocalizedString("VERSION_LABEL", comment: "").replacingOccurrences(of: "%1", with: p1).replacingOccurrences(of: "%2", with: p2) }
 	static let ZONE_DK1 = NSLocalizedString("ZONE_DK1", comment: "")
 	static let ZONE_DK2 = NSLocalizedString("ZONE_DK2", comment: "")
 }
