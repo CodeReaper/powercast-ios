4d035d61b51107a32e91e9e13de8866d5766b475
Jakob Jensen <jakobj@jakobj.dk>
2023-12-18 20:50:16 +0100

    Upgrade dependencies

--- Powercast/UI/Common/LoadingView.swift
+++ Powercast/UI/Common/LoadingView.swift
@@ -3,17 +3,19 @@ import Lottie
 import SugarKit
 
 struct View {
-    static func buildLoadingView(color: UIColor = .black) -> AnimationView {
+    static func buildLoadingView(color: UIColor = .black) -> LottieAnimationView {
         var red = CGFloat.zero
         var green = CGFloat.zero
         var blue = CGFloat.zero
         var alpha = CGFloat.zero
         color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
 
-        let view = AnimationView(name: Animations.connecting)
+        let view = LottieAnimationView(name: Animations.connecting)
         view.loopMode = .loop
         view.setValueProvider(
-            ColorValueProvider(Lottie.Color(r: red, g: green, b: blue, a: alpha, denominator: .One)),
+            ColorValueProvider(
+                LottieColor(r: red, g: green, b: blue, a: alpha, denominator: .One)
+            ),
             keypath: AnimationKeypath(keypath: "**.Color")
         )
         return view
