2afc04e63fd84b1e7af90023643a696b742a9326
Jakob Jensen <jakobj@jakobj.dk>
2023-12-21 21:57:34 +0100

    Clean up unused resources (#61)

--- Powercast/Assets/Fonts.swift
+++ Powercast/Assets/Fonts.swift
@@ -8,24 +8,6 @@ struct Font {
         }
     }
 
-    private static let cooperHewittFonts: [UIFont.TextStyle: UIFont] = [
-        .largeTitle: UIFont(name: "CooperHewitt-Book", size: 34)!,
-        .title1: UIFont(name: "CooperHewitt-Book", size: 28)!,
-        .title2: UIFont(name: "CooperHewitt-Book", size: 22)!,
-        .title3: UIFont(name: "CooperHewitt-Book", size: 20)!,
-        .headline: UIFont(name: "CooperHewitt-Bold", size: 17)!,
-        .body: UIFont(name: "CooperHewitt-Book", size: 17)!,
-        .callout: UIFont(name: "CooperHewitt-Book", size: 16)!,
-        .subheadline: UIFont(name: "CooperHewitt-Book", size: 15)!,
-        .footnote: UIFont(name: "CooperHewitt-Book", size: 13)!,
-        .caption1: UIFont(name: "CooperHewitt-Book", size: 12)!,
-        .caption2: UIFont(name: "CooperHewitt-Book", size: 11)!
-    ]
-
-    static func cooperHewitt(for style: UIFont.TextStyle) -> UIFont {
-        return UIFontMetrics(forTextStyle: style).scaledFont(for: cooperHewittFonts[style]!)
-    }
-
     static func alexBrush(size: CGFloat) -> UIFont {
         return UIFont(name: "AlexBrush-Regular", size: size)!
     }
