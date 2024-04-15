7cb881513ac6380a7534f6e8d4e708ed7201cc83
Jakob Jensen <jakobj@jakobj.dk>
2024-04-15 23:10:34 +0200

    Show price data when emission data is missing

--- Powercast/UI/Common/PriceHeaderView.swift
+++ Powercast/UI/Common/PriceHeaderView.swift
@@ -8,7 +8,9 @@ class PriceHeaderView: UITableViewHeaderFooterView {
     private let dateLabel = Label(style: .body, color: .cellHeaderText)
     private let pricesLabel = Label(style: .body, color: .cellHeaderText).aligned(to: .right)
     private let rawLabel = Label(style: .body, color: .cellHeaderText).aligned(to: .right)
-    private let emissionLabel = Label(style: .body, color: .cellHeaderText).aligned(to: .right)
+    private let emissionRangeLabel = Label(style: .body, color: .cellHeaderText).aligned(to: .right)
+    private let emissionUnitLabel = Label(style: .body, text: Translations.CO2_UNIT, color: .secondaryLabelText)
+    private let emissionTitleLabel = Label(style: .body, text: Translations.DASHBOARD_CO2_VARIATION, color: .secondaryLabelText)
 
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
@@ -23,14 +25,14 @@ class PriceHeaderView: UITableViewHeaderFooterView {
                 spacing: 5,
                 dateLabel,
                 Label(style: .body, text: Translations.DASHBOARD_RAW_PRICE_VARIATION, color: .secondaryLabelText),
-                Label(style: .body, text: Translations.DASHBOARD_CO2_VARIATION, color: .secondaryLabelText)
+                emissionTitleLabel
             ),
             Stack.views(
                 on: .vertical,
                 spacing: 5,
                 Stack.views(spacing: 3, pricesLabel, Label(style: .body, text: Translations.COST_UNIT, color: .secondaryLabelText)),
                 Stack.views(spacing: 3, rawLabel, Label(style: .body, text: Translations.COST_UNIT, color: .secondaryLabelText)),
-                Stack.views(spacing: 3, emissionLabel, Label(style: .body, text: Translations.CO2_UNIT, color: .secondaryLabelText))
+                Stack.views(spacing: 3, emissionRangeLabel, emissionUnitLabel)
             )
         ).setup(in: contentView)
     }
@@ -43,8 +45,13 @@ class PriceHeaderView: UITableViewHeaderFooterView {
         dateLabel.text = Self.dateFormatter.string(from: price.duration.lowerBound)
         pricesLabel.text = Translations.SPAN(Self.numberFormatter.string(with: price.priceSpan.lowerBound), Self.numberFormatter.string(with: price.priceSpan.upperBound))
         rawLabel.text = Self.numberFormatter.string(with: price.rawPriceSpan.upperBound - price.rawPriceSpan.lowerBound)
-        emissionLabel.text = Translations.SPAN(Self.numberFormatter.string(with: emission?.amountSpan.lowerBound ?? 0), Self.numberFormatter.string(with: emission?.amountSpan.upperBound ?? 0))
-        emissionLabel.set(hidden: emission == nil)
+
+        let hasEmissions = emission != nil
+        emissionTitleLabel.set(hidden: !hasEmissions)
+        emissionUnitLabel.set(hidden: !hasEmissions)
+        emissionRangeLabel.set(hidden: !hasEmissions)
+        emissionRangeLabel.text = Translations.SPAN(Self.numberFormatter.string(with: emission?.amountSpan.lowerBound ?? 0), Self.numberFormatter.string(with: emission?.amountSpan.upperBound ?? 0))
+
         return self
     }
 }
